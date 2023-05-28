9372efcc1036e3f623c8b3eb6a1138f746848321
tejbuwa06 <134685025+tejbuwa06@users.noreply.github.com>
2023-05-28 21:26:52 +0530

    Add files via upload

---
+++ Day2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int len,bred;
+     
+cout<<"Enter the length and bredth: ";
+cin>>len>>bred;
+cout<<"The area is : "<<len*bred <<endl;
+cout<<"tHE Perimeter is : "<<2*(len+bred)<<endl;
+
+
+}
