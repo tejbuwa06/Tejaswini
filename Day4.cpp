848a9ec0d598391f4c2280eb979d9059f444f744
tejbuwa06 <134685025+tejbuwa06@users.noreply.github.com>
2023-05-31 08:47:34 +0530

    Add files via upload

---
+++ Day4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    char c;
+    float t;
+    double d;
+
+cout<<"The size of int:  "<<sizeof(i)<<endl;
+cout<<"The size of char : "<<sizeof(c)<<endl;
+cout<<"The size of float  : "<<sizeof(t)<<endl;
+cout<<"The size of double :  "<<sizeof(d)<<endl;
+
+cout<<"Enter the character : "<<endl;
+cin>>c;
+cout<<"The asci value of character is : "<<int(c)<<endl;
+
+
+
+}
